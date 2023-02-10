46c14cf5618e8012e350c4c158f3a46bb6fa1255
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2023-02-09 23:51:24 -0700

    Delete build-radiocoin-linux.sh

--- build-radiocoin-linux.sh
+++
@@ -1,6 +0,0 @@
-sh autogen.sh
- ./configure --with-incompatible-bdb --prefix=/usr --enable-sse2
-echo 'adjust -j24 to your system core count to avoid compiler panics check with htop or etcetcetc..
-make -j24
-make -j24 install
-'
