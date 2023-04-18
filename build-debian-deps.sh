eb4e89c0f675837c81cee490604daa64c54ffa39
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2023-04-17 20:47:57 -0700

    Update build-debian-deps.sh

--- build-debian-deps.sh
+++ build-debian-deps.sh
@@ -7,5 +7,5 @@ sudo apt install libtool autoconf -y
     sudo apt-get install libzmq3-dev -y
     sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler -y
     sudo apt-get install libqt4-dev libprotobuf-dev protobuf-compiler -y
-    sudo apt-get install libqrencode-dev libdb5.3-dev -y
+    sudo apt-get install libqrencode-dev -y
 
