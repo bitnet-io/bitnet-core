25b298a3bb6663ae1b5c945d7262f12b706bffbe
Your Name <you@example.com>
2023-12-01 01:23:25 -0700

    push to fix the build system added build-linux-static.sh

---
+++ build-linux-static.sh
@@ -0,0 +1,18 @@
+echo 'you can change this file for 8 cores with make -j8
+cd depends
+make -j8 HOST=x86_64-pc-linux-gnu
+cd ..
+./autogen.sh
+CONFIG_SITE=$PWD/depends/x86_64-pc-linux-gnu/share/config.site ./configure
+make -j8
+'
+sleep 1s
+echo 'will not build the binary using 1 core'
+sleep 1s
+
+cd depends
+make HOST=x86_64-pc-linux-gnu
+cd ..
+./autogen.sh
+CONFIG_SITE=$PWD/depends/x86_64-pc-linux-gnu/share/config.site ./configure
+make 
