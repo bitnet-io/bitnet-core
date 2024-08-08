2d939285452de5e3c3a656ef696dc568a3f77548
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2024-08-08 06:14:51 -0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -107,13 +107,11 @@ public:
 
 
         consensus.powLimit = uint256S      ("000000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-        consensus.posLimit = uint256S      ("0000000ffff0fffffffffffffffffffffffffffffffffffffffffffffffffffff");
-        consensus.QIP9PosLimit = uint256S  ("0000000ffff0fffffffffffffffffffffffffffffffffffffffffffffffffffff"); // The new POS-limit activated after QIP9
-        consensus.RBTPosLimit = uint256S   ("0000000ffff0fffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        consensus.posLimit = uint256S      ("0000000000000ffff0fffffffffffffffffffffffffffffffffffffffffffffff");
+        consensus.QIP9PosLimit = uint256S  ("0000000000000ffff0fffffffffffffffffffffffffffffffffffffffffffffff"); // The new POS-limit activated after QIP9
+        consensus.RBTPosLimit = uint256S   ("0000000000000ffff0fffffffffffffffffffffffffffffffffffffffffffffff");
 
-//        consensus.posLimit = uint256S      ("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-//        consensus.QIP9PosLimit = uint256S  ("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); // The new POS-limit activated after QIP9
-//        consensus.RBTPosLimit = uint256S   ("0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+
 
         consensus.nPowTargetTimespan = 24 * 60 * 60; // 16 minutes
         consensus.nPowTargetTimespanV2 = 24 * 60 * 60;
